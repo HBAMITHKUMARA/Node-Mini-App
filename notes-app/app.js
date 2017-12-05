654669a89003759ac845735c90e1fc8271573ef5
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-05 22:03:29 +0530

    notes app

---
+++ notes-app/app.js
@@ -0,0 +1,21 @@
+console.log('Starting app...');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes');
+
+var user = os.userInfo();
+console.log('Hi ' + user.username + '!!!! Here is user details...');
+console.log(user);
+
+// fs.appendFile('greeting.txt', 'Hello Wold!!!!!');
+
+fs.appendFileSync('greeting.txt', 'Hello Wold!!!!!');
+//
+// fs.appendFile('greeting.txt', 'Hello Wold!!!!!', function(err) {
+//    if(err) {
+//        console.log('error:  ', err);
+//    }
+// });
+
+
