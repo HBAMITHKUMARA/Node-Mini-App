1730e04f11e5d86ec296100fb11321363fe54a30
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-06 23:55:42 +0530

    minor code changes and added package.json

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,12 +5,16 @@ const os = require('os');
 const notes = require('./notes');
 
 var user = os.userInfo();
-console.log('Hi ' + user.username + '!!!! Here is user details...');
-console.log(user);
+// console.log('Hi ' + user.username + '!!!! Here is user details...');
+// console.log(user);
+
+fs.appendFileSync('greeting.txt', 'Hello Wold!!!!!');
+
+console.log(notes.addNote());
+console.log(notes.add(4,2));
 
 // fs.appendFile('greeting.txt', 'Hello Wold!!!!!');
 
-fs.appendFileSync('greeting.txt', 'Hello Wold!!!!!');
 //
 // fs.appendFile('greeting.txt', 'Hello Wold!!!!!', function(err) {
 //    if(err) {
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,1 +1,16 @@
 console.log('Starting notes...');
+
+console.log(module);
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    console.log(module);
+    return 'New note';
+}
+
+module.exports.add = (a, b) => {
+    console.log(a + b);
+    console.log(module);
+    return 'sum=' + (a + b);
+}
+
