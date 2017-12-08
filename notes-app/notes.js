7989a58220c1914e34cccb63eb2cfdc26a214916
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-08 22:26:48 +0530

    code enhancement to save and remove note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,19 +1,56 @@
 console.log('Starting notes...');
 
+const fs = require('fs');
+
+let fetchNotes = () => {
+    try{
+        let stringNotes = fs.readFileSync('notes-data.json');
+        return JSON.parse(stringNotes);
+    }catch(e){
+        console.log('Error in reading/Empty file');
+        return [];
+    }
+}
+
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 var addNote = (title, body) => {
-    console.log('Adding note: ', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title: title,
+        body: body
+    };
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+    }
 };
 
 var getAll = () => {
     console.log('Get all notes');
+    console.log(fetchNotes());
 };
 
 var getNote = (title) => {
   console.log('Reading note: ' + title);
+  notes = fetchNotes();
+  note = notes.filter((note) => {
+   if(note.title === title){
+       return note;
+   }
+  });
+  console.log(note);
 };
 
 var removeNote = (title) => {
-    console.log('Removing note: ' + title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+
+    return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
