d0714454e935ee039ae6e6008d39d6842a46e075
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-09 12:39:29 +0530

    added new hack: arrow functions

--- hacks/app.js
+++ hacks/app.js
@@ -1,7 +1,9 @@
 console.log('Starting app...');
 
 // node app.js --title="ddd" --ami="ff"
-const cmd_hacks = require('./cmd-hacks');
+// const cmd_hacks = require('./cmd-hacks');
+//
+// const json_hacks = require('./json-hacks');
 
-// no i/p
-const json_hacks = require('./json-hacks');
+const arrow_function_hacks = require('./arrow-function-hacks');
+
