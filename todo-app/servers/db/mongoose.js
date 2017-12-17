fb2b190cf3e2266e12c8527a1bea1300676f915d
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-17 16:39:28 +0530

    add DELETE /todos/:id

--- todo-app/servers/db/mongoose.js
+++ todo-app/servers/db/mongoose.js
@@ -3,7 +3,7 @@ console.log('Starting mongoose...');
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todosApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todosApp');
 
 module.exports = {
     mongoose
