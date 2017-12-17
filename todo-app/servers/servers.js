f42b0de0c2e2296e8ca077d2e6a867fd4b57022b
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-17 13:48:44 +0530

    minor fix

--- todo-app/servers/servers.js
+++ todo-app/servers/servers.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send({error: 'id not found'});
         }
-        res.send(todo);
+        res.send({todo});
     }, (err) => {
         res.status(400).send(err);
     });
