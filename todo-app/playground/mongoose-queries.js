2106536dbb030880227245f4cf419e653084b3cd
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-17 13:17:12 +0530

    code refactor

--- todo-app/playground/mongoose-queries.js
+++ todo-app/playground/mongoose-queries.js
@@ -6,36 +6,36 @@ const {mongoose} = require('./../servers/db/mongoose');
 const {Todo} = require('./../servers/models/todo');
 const {User} = require('./../servers/models/user');
 
-let id = '5a357b450fa326c80e7fe9b0';
+// let id = '5a357b450fa326c80e7fe9b0';
 
-if(!ObjectId.isValid(id)){
-    console.log('id is not valid');
-}
+// if(!ObjectId.isValid(id)){
+//     console.log('id is not valid');
+// }
 
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//     console.log('todos: ', todos);
-// });
-//
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     console.log('todo:  ', todo);
-// });
-//
-// Todo.findById(id).then((todo) => {
-//     if(!todo){
-//         console.log('id not found');
-//     }
-//     console.log('todo:  ', todo);
-// });
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('todos: ', todos);
+});
 
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('todo:  ', todo);
+});
 
+Todo.findById(id).then((todo) => {
+    if(!todo){
+        console.log('id not found');
+    }
+    console.log('todo:  ', todo);
+});
 
 
 
-id = '5a352343642dd2300abaa373';
+
+
+// id = '5a352343642dd2300abaa373';
 
 User.find().then((users) => {
     console.log('users: ', users);
