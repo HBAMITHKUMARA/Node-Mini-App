5f09718f5d21dea4a6e17c9da89607c0b6deb355
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-18 01:19:54 +0530

    user model configured and added jwt, validator modules

--- todo-app/servers/models/user.js
+++ todo-app/servers/models/user.js
@@ -1,14 +1,35 @@
 console.log('Starting user...');
 
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 let User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 7
-    }
+        minlength: 7,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
 });
 
 module.exports = {
