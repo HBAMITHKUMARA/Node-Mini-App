826248c7ec303af86732f00c209ff068745e2e82
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-18 23:57:25 +0530

    code refactor for jwt configuration

--- todo-app/servers/servers.js
+++ todo-app/servers/servers.js
@@ -110,10 +110,16 @@ app.post('/users', (req, res) => {
     let user = new User(body);
 
     user.save()
-        .then((docs) => {
-            res.send(docs);
+        .then(() => {
+            console.log('damn1');
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            console.log('damn2');
+            res.header('x-auth', token).send(user);
         })
         .catch((err) => {
+            console.log('damn3');
             res.status(400).send(err);
         });
 });
