bedeb785c4fbe2068393f410c6a6675207a95d49
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-19 23:52:07 +0530

    hash technique using bcrypt

--- todo-app/playground/hashing.js
+++ todo-app/playground/hashing.js
@@ -1,8 +1,17 @@
 console.log('Starting hashing...');
 
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
-let message = 'i am ami';
+let message = 'hbamithkumara';
 let hash = SHA256(message).toString();
 
-console.log(hash);
+console.log('hbamithkumara: ', hash);
+
+password = 'testabc1'
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash:  ', hash);
+    });
+});
