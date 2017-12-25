4ca6fb2dd89732f40b7181d1583a6f1f8125c3cd
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-25 22:06:31 +0530

    node chat app

---
+++ chat-app/server/server.js
@@ -0,0 +1,30 @@
+console.log('Starting server...');
+
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname + '/../public');
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket)=> {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+});
+
+
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
