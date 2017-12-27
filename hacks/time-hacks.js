b461fe93ca893e7fc38471b24f388cfa9937366a
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-27 23:56:26 +0530

    added date-time hacks

---
+++ hacks/time-hacks.js
@@ -0,0 +1,22 @@
+console.log('Starting time-hacks...');
+
+const moment = require('moment');
+
+console.log('general method');
+let date1 = new Date();
+console.log(date1.getMonth());
+
+
+console.log('better method');
+let date = moment();
+console.log(date.format());
+console.log(date.format('MMM'));
+console.log(date.format('YYYY'));
+console.log(date.format('Do'));
+console.log(date.format('DD'));
+console.log(date.format('MMM YYYY'));
+console.log(date.format('MMM YYYY DD'));
+
+
+date.add(100, 'year');
+console.log(date.format('MMM YYYY DD'));
