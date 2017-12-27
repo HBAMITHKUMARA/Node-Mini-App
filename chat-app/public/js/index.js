176b4615234bdd79de11131149ee9f2fd8a6a922
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-12-27 23:01:39 +0530

    generate location message event

--- chat-app/public/js/index.js
+++ chat-app/public/js/index.js
@@ -11,9 +11,17 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
-    let li = jQuery('<li></li>')
+    let li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+    let li = jQuery('<li></li>');
+    let a = jQuery('<a target="_blank">My current location</a>')
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
     jQuery('#messages').append(li);
 });
 
@@ -25,5 +33,22 @@ jQuery('#message-form').on('submit', function(e) {
         text: jQuery('[name=message]').val()
     }, function() {
         console.log('got it');
+    });
+});
+
+let locationButton = jQuery('#send-location');
+
+locationButton.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('geolocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function() {
+        alert('unable to fetch the location');
     });
 });
